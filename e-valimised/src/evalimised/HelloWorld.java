2bd5d15e71e2bb27f71f2780179ad95ffd0d6fc9
Jakob Mass <jaks@ut.ee>
2013-03-26 23:41:17 +0200

    commit to test changes

--- e-valimised/src/evalimised/HelloWorld.java
+++ e-valimised/src/evalimised/HelloWorld.java
@@ -14,7 +14,7 @@ import java.sql.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+//comment to test commit
 public class HelloWorld extends HttpServlet {
 	
 	@Override
